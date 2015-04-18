96f39ce9071264cfd0d1ac781e57f1dca2a4242b
Hugh Lee <hugh@tapasmedia.co>
2015-04-18 17:01:03 +0900

    Add docs / Throw exception for wrong userId

--- src/main/java/com/sangdol/experiment/portableDb/ViewTable.java
+++ src/main/java/com/sangdol/experiment/portableDb/ViewTable.java
@@ -1,9 +1,12 @@
 package com.sangdol.experiment.portableDb;
 
+import javax.ws.rs.WebApplicationException;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Manage horizontally split table names.
+ *
  * @author hugh
  */
 public class ViewTable {
@@ -17,14 +20,24 @@ public class ViewTable {
         }
     }
 
+    /**
+     * Returns all table names with designated TABLE_PREFIX and TABLE_COUNT.
+     */
     public List<String> getAll() {
         return names;
     }
 
-    // TODO throw exception when trying to get a not existing table
+    /**
+     * Returns the allocated table name to the userId.
+     * The index of table name is 0-based, e.g. true_view0, true_view1...
+     */
     public String get(int userId) {
         final int HOST_COUNT_IN_TABLE = 100;
         int tableNumber = (userId - 1) / HOST_COUNT_IN_TABLE;
+
+        if (tableNumber >= TABLE_COUNT)
+            throw new WebApplicationException(404);
+
         return TABLE_PREFIX + tableNumber;
     }
 }
