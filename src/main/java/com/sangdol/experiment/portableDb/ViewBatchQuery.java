ce4f7f41f0d6ced0175140eb2490f8c1bf0c7388
Hugh Lee <hugh@tapasmedia.co>
2015-11-15 17:21:02 +0900

    Upgrade to Java 1.8

--- src/main/java/com/sangdol/experiment/portableDb/ViewBatchQuery.java
+++ src/main/java/com/sangdol/experiment/portableDb/ViewBatchQuery.java
@@ -2,6 +2,7 @@ package com.sangdol.experiment.portableDb;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 /**
  * Batch SQL queries generating class.
@@ -24,17 +25,13 @@ public class ViewBatchQuery {
         this.viewTable = viewTable;
     }
 
-    interface ViewBatchQueryStrategy {
-        String getQueries(List<String> strings);
-    }
-
-    private List<String> getQueriesList(ViewBatchQueryStrategy strategy) {
+    private List<String> getQueriesList(Function<List<String>, String> func) {
         List<String> list = new ArrayList<>();
         List<String> tables = viewTable.getAll();
 
         for (int i = 0; i < tables.size(); i += QUERY_COUNT_AT_ONCE) {
             int to = Math.min(i + QUERY_COUNT_AT_ONCE, tables.size());
-            list.add(strategy.getQueries(tables.subList(i, to)));
+            list.add(func.apply(tables.subList(i, to)));
         }
 
         return list;
@@ -46,27 +43,24 @@ public class ViewBatchQuery {
      * - Create Index http://www.h2database.com/html/grammar.html#create_index
      */
     public List<String> getCreateTablesList() {
-        return getQueriesList(new ViewBatchQueryStrategy() {
-            @Override
-            public String getQueries(List<String> tables) {
-                StringBuilder sb = new StringBuilder();
-                for (String table : tables) {
-                    // Create Table
-                    sb.append(" CREATE TABLE ").append(table);
-                    sb.append(" ( ");
-                    sb.append(" id BIGINT PRIMARY KEY AUTO_INCREMENT, ");
-                    sb.append(" host_id INT, ");
-                    sb.append(" visitor_id INT, ");
-                    sb.append(" date TIMESTAMP ");
-                    sb.append(" ); ");
-                    // Create Index
-                    // It's not possible to add indices while creating tables with H2.
-                    sb.append(" CREATE INDEX ON ").append(table);
-                    sb.append(" (host_id); ");
-                }
-
-                return sb.toString();
+        return getQueriesList(tables -> {
+            StringBuilder sb = new StringBuilder();
+            for (String table : tables) {
+                // Create Table
+                sb.append(" CREATE TABLE ").append(table);
+                sb.append(" ( ");
+                sb.append(" id BIGINT PRIMARY KEY AUTO_INCREMENT, ");
+                sb.append(" host_id INT, ");
+                sb.append(" visitor_id INT, ");
+                sb.append(" date TIMESTAMP ");
+                sb.append(" ); ");
+                // Create Index
+                // It's not possible to add indices while creating tables with H2.
+                sb.append(" CREATE INDEX ON ").append(table);
+                sb.append(" (host_id); ");
             }
+
+            return sb.toString();
         });
     }
 
@@ -74,18 +68,15 @@ public class ViewBatchQuery {
      * Returns queries which select count(*) of all tables.
      */
     public List<String> getSelectCountsList() {
-        return getQueriesList(new ViewBatchQueryStrategy() {
-            @Override
-            public String getQueries(List<String> tables) {
-                String UNION = "";
-                StringBuilder sb = new StringBuilder();
-                for (String table : tables) {
-                    sb.append(UNION).append(" SELECT COUNT(*) FROM ").append(table);
-                    UNION = " UNION ALL ";
-                }
-
-                return sb.toString();
+        return getQueriesList(tables -> {
+            String UNION = "";
+            StringBuilder sb = new StringBuilder();
+            for (String table : tables) {
+                sb.append(UNION).append(" SELECT COUNT(*) FROM ").append(table);
+                UNION = " UNION ALL ";
             }
+
+            return sb.toString();
         });
     }
 
@@ -98,22 +89,19 @@ public class ViewBatchQuery {
      * to remove nested select statement.
      */
     public List<String> getDeleteTablesList() {
-        return getQueriesList(new ViewBatchQueryStrategy() {
-            @Override
-            public String getQueries(List<String> tables) {
-                StringBuilder sb = new StringBuilder();
-                for (String table : tables) {
-                    sb.append(" DELETE FROM ").append(table);
-                    sb.append(" where id IN ");
-                    sb.append(" (SELECT id FROM ");
-                    sb.append(" (SELECT id, (SELECT COUNT(*) FROM ").append(table);
-                    sb.append(" v2 WHERE v1.id <= v2.id AND v1.host_id = v2.host_id) AS rank ");
-                    sb.append(" FROM ").append(table);
-                    sb.append(" v1 ORDER BY id DESC) sub ");
-                    sb.append(" WHERE rank > 10); ");
-                }
-                return sb.toString();
+        return getQueriesList(tables -> {
+            StringBuilder sb = new StringBuilder();
+            for (String table : tables) {
+                sb.append(" DELETE FROM ").append(table);
+                sb.append(" where id IN ");
+                sb.append(" (SELECT id FROM ");
+                sb.append(" (SELECT id, (SELECT COUNT(*) FROM ").append(table);
+                sb.append(" v2 WHERE v1.id <= v2.id AND v1.host_id = v2.host_id) AS rank ");
+                sb.append(" FROM ").append(table);
+                sb.append(" v1 ORDER BY id DESC) sub ");
+                sb.append(" WHERE rank > 10); ");
             }
+            return sb.toString();
         });
     }
 }
